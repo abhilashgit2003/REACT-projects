2e9b8291f47d5d64354c3fd664ea948700627c78
abhilashgit2003 <hemanthdavarasingi11@gmail.com>
2025-09-15 12:50:36 +0530

    Broked the code into components

--- Projects/todo-app/src/App.jsx
+++ Projects/todo-app/src/App.jsx
@@ -1,42 +1,16 @@
+import AppName from "./Components/AppName";
+import AddTodos from "./Components/AddTodo";
+import AddTodo from "./Components/AddTodo";
+import TodoItem1 from "./Components/TodoItem1";
+import TodoItem2 from "./Components/TodoItem2";
+
 function App() {
   return (
     <center class="todo-container">
-      <h1>Todo App</h1>
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Enter the task" name="" id="" />
-          </div>
-          <div class="col-4">
-            <input type="date" />
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">
-              Add
-            </button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-6">Buy Milk</div>
-          <div class="col-4">4/12/2003</div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-6">Go for a walk</div>
-          <div class="col-4">4/12/2003</div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
+      <AppName />
+      <AddTodo />
+      <TodoItem1 />
+      <TodoItem2 />
     </center>
   );
 }
