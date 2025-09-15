4a977882f14050b3f7b011abc6bd3a0b252902d8
abhilashgit2003 <hemanthdavarasingi11@gmail.com>
2025-09-15 13:17:21 +0530

    Addes basic css

--- Projects/todo-app/src/App.jsx
+++ Projects/todo-app/src/App.jsx
@@ -3,14 +3,17 @@ import AddTodos from "./Components/AddTodo";
 import AddTodo from "./Components/AddTodo";
 import TodoItem1 from "./Components/TodoItem1";
 import TodoItem2 from "./Components/TodoItem2";
+import "./App.css";
 
 function App() {
   return (
-    <center class="todo-container">
+    <center className="todo-container">
       <AppName />
       <AddTodo />
-      <TodoItem1 />
-      <TodoItem2 />
+      <div className="items-container">
+        <TodoItem1 />
+        <TodoItem2 />
+      </div>
     </center>
   );
 }
--- Projects/todo-app/src/Components/TodoItem2.jsx
+++ Projects/todo-app/src/Components/TodoItem2.jsx
@@ -3,12 +3,12 @@ function TodoItem2() {
   let todoDate = "4/12/2003";
 
   return (
-    <div class="container text-center">
-      <div class="row">
+    <div class="container ">
+      <div class="row kg-row">
         <div class="col-6">{todoName}</div>
         <div class="col-4">{todoDate}</div>
         <div class="col-2">
-          <button type="button" class="btn btn-danger">
+          <button type="button" class="btn btn-danger kg-button">
             Delete
           </button>
         </div>
